9d416855a2bb1e07e54052ec8fcbc1e9e24dc531
AjithMP <ajithrocks1997@gmail.com>
2021-05-30 01:49:40 +0530

    First Commit

---
+++ src/main/java/com/ajith/chatapp/domain/ChatMessagePojo.java
@@ -0,0 +1,38 @@
+package com.ajith.chatapp.domain;
+
+public class ChatMessagePojo {
+
+	 private MessageType type;
+	    private String content;
+	    private String sender;//username
+
+	    public enum MessageType {
+	        CHAT,
+	        JOIN,
+	        LEAVE
+	    }
+
+	    public MessageType getType() {
+	        return type;
+	    }
+
+	    public void setType(MessageType type) {
+	        this.type = type;
+	    }
+
+	    public String getContent() {
+	        return content;
+	    }
+
+	    public void setContent(String content) {
+	        this.content = content;
+	    }
+
+	    public String getSender() {
+	        return sender;
+	    }
+
+	    public void setSender(String sender) {
+	        this.sender = sender;
+	    }
+	}
